19b433619d599da5a612b7f3b3b5bfef0c5a9731
Natalie Burgei <natalieburgei04@gmail.com>
2023-12-13 13:57:51 +0300

    completed asssignment

--- lib/person.py
+++ lib/person.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 APPROVED_JOBS = [
     "Admin",
     "Customer Service",
@@ -16,4 +14,29 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Guido", job="Sales"):
+        self._name = ""  # Initialize _name as an empty string
+        self._job = ""
+        self.set_name(name)
+        self.set_job(job)
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name=""):
+        if type(name) == str and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job=""):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
